04c3260944adb48a549102f72b61a7b3b02d2392
Alex Snaps <alex.snaps@gmail.com>
2014-09-05 14:18:48 -0400

    Made CacheBuilder StandaloneCacheBuilder and made it extensible

---
+++ core/src/main/java/org/ehcache/StandaloneCacheBuilder.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Terracotta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ehcache;
+
+import org.ehcache.config.StandaloneCacheConfiguration;
+import org.ehcache.spi.ServiceProvider;
+import org.ehcache.spi.cache.CacheProvider;
+
+/**
+ * @author Alex Snaps
+ */
+public class StandaloneCacheBuilder<K, V, T extends StandaloneCache<K, V>> {
+
+  private final Class<K> keyType;
+  private final Class<V> valueType;
+
+  public StandaloneCacheBuilder(final Class<K> keyType, final Class<V> valueType) {
+    this.keyType = keyType;
+    this.valueType = valueType;
+  }
+
+  T build(ServiceProvider serviceProvider) {
+    return (T)serviceProvider.findService(CacheProvider.class).createCache(keyType, valueType);
+  }
+
+  public final T build() {
+    return build(new ServiceProvider());
+  }
+
+  public final <N extends T> StandaloneCacheBuilder<K, V, N> with(StandaloneCacheConfiguration<K, V, N> cfg) {
+    return cfg.builder(this);
+  }
+
+  public static <K, V, T extends StandaloneCache<K, V>> StandaloneCacheBuilder<K, V, T> newCacheBuilder(Class<K> keyType, Class<V> valueType) {
+    return new StandaloneCacheBuilder<K, V, T>(keyType, valueType);
+  }
+
+}
