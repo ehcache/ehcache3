ec45378dbb87cfaf303777e0920c27c4bec58d9f
Alex Snaps <alex.snaps@gmail.com>
2014-09-23 13:26:34 -0400

    Issue #62 Not binding <K,V> at the StoreTester level

--- impl/src/test/java/org/ehcache/OnHeapStoreTest.java
+++ impl/src/test/java/org/ehcache/OnHeapStoreTest.java
@@ -2,7 +2,7 @@ package org.ehcache;
 
 import org.ehcache.internal.store.OnHeapStore;
 import org.ehcache.spi.cache.Store;
-import org.ehcache.spi.test.Factory;
+import org.ehcache.spi.test.StoreFactory;
 import org.ehcache.spi.test.StoreTester;
 import org.junit.Test;
 
@@ -10,10 +10,15 @@ public class OnHeapStoreTest {
 
   @Test
   public void test() throws Exception {
-    StoreTester<String, String> tester = new StoreTester<String, String>(new Factory<Store<String, String>>() {
+    StoreTester tester = new StoreTester(new StoreFactory() {
       @Override
       public Store<String, String> newInstance() {
         return new OnHeapStore<String, String>();
+      }
+
+      @Override
+      public <K, V> Store<K, V> newStore(final Class<K> keyType, final Class<V> valueType) {
+        return new OnHeapStore<K, V>();
       }
     });
     tester.runTestSuite().reportAndThrow();
