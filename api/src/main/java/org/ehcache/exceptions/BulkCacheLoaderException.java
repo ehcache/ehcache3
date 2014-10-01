8fe06ab649a0f59a50067c6187f4d831012b08b0
Alex Snaps <alex.snaps@gmail.com>
2014-09-30 21:34:48 -0400

    Issue #29 Added missing Javadoc

--- api/src/main/java/org/ehcache/exceptions/BulkCacheLoaderException.java
+++ api/src/main/java/org/ehcache/exceptions/BulkCacheLoaderException.java
@@ -20,6 +20,9 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
+ * Exception thrown by a {@link org.ehcache.Cache} when the {@link org.ehcache.spi.loader.CacheLoader} it uses threw an
+ * {@link java.lang.RuntimeException} while bulk loading values for a given set of keys
+ *
  * @author Alex Snaps
  */
 public class BulkCacheLoaderException extends CacheLoaderException {
@@ -28,9 +31,12 @@ public class BulkCacheLoaderException extends CacheLoaderException {
   private final Map<?, ?> successes;
 
   /**
+   * Constructs a new BulkCacheLoaderException providing the key set that failed, including the exception loading these
+   * threw, as well as all keys we managed to load a value for, including the value loaded. This latter set of keys was
+   * loaded successfully into the {@link org.ehcache.Cache}.
    *
-   * @param failures
-   * @param successes
+   * @param failures the map of keys to failure encountered while loading the values
+   * @param successes the map of keys successfully loaded and their associated values
    */
   public BulkCacheLoaderException(final Map<?, Exception> failures, final Map<?, ?> successes) {
     this.failures = Collections.unmodifiableMap(failures);
@@ -38,10 +44,12 @@ public class BulkCacheLoaderException extends CacheLoaderException {
   }
 
   /**
+   * {@inheritDoc}
    *
-   * @param message
-   * @param failures
-   * @param successes
+   * @param message the message
+   * @param failures the map of keys to failure encountered while loading the values
+   * @param successes the map of keys successfully loaded and their associated values
+   * @see org.ehcache.exceptions.BulkCacheLoaderException#BulkCacheLoaderException(java.util.Map, java.util.Map)
    */
   public BulkCacheLoaderException(final String message, final Map<Object, Exception> failures, final Map<Object, Object> successes) {
     super(message);
@@ -50,16 +58,18 @@ public class BulkCacheLoaderException extends CacheLoaderException {
   }
 
   /**
-   *
-   * @return
+   * Accessor to all keys that failed loading during a bulk load operation, with the associated
+   * {@link java.lang.Exception} encountered
+   * @return a map of keys to exception encountered during bulk load
    */
   public Map<?, Exception> getFailures() {
     return failures;
   }
 
   /**
-   *
-   * @return
+   * Accessor to all keys that were successfully loaded during a bulk load operation, with the associated
+   * loaded value
+   * @return a map of keys to value loaded and installed in the {@link org.ehcache.Cache}
    */
   public Map<?, ?> getSuccesses() {
     return successes;
--- api/src/main/java/org/ehcache/spi/loader/CacheLoaderFactory.java
+++ api/src/main/java/org/ehcache/spi/loader/CacheLoaderFactory.java
@@ -24,7 +24,12 @@ import org.ehcache.spi.service.ServiceConfiguration;
  * A factory {@link org.ehcache.spi.service.Service} that will create {@link org.ehcache.spi.loader.CacheLoader}
  * instances for a given {@link org.ehcache.Cache} managed by a {@link org.ehcache.CacheManager}
  *
- * The {@link org.ehcache.CacheManager} will an instance of this Class prior to
+ * The {@link org.ehcache.CacheManager} will request an instance of this Class prior to creating any
+ * {@link org.ehcache.Cache} instances. It'll then use this instance to create
+ * {@link org.ehcache.spi.loader.CacheLoader} instances for each {@link org.ehcache.Cache} it manages by
+ * invoking the {@link #createLoader(String, org.ehcache.config.CacheConfiguration)} method. For any non {@code null}
+ * value returned, the {@link org.ehcache.Cache} will be configured to use the
+ * {@link org.ehcache.spi.loader.CacheLoader} instance returned.
  *
  * @author Alex Snaps
  */
@@ -32,17 +37,18 @@ public interface CacheLoaderFactory extends Service {
 
   /**
    * Invoked by the {@link org.ehcache.CacheManager} when a {@link org.ehcache.Cache} is being added to it.
-   * @param alias the {@link org.ehcache.Cache} instance's alias in the CacheManager
-   * @param cacheConfiguration
-   * @param <K>
-   * @param <V>
-   * @return
+   * @param alias the {@link org.ehcache.Cache} instance's alias in the {@link org.ehcache.CacheManager}
+   * @param cacheConfiguration the configuration instance that will be used to create the {@link org.ehcache.Cache}
+   * @param <K> the key type for the associated {@link org.ehcache.Cache}
+   * @param <V> the value type for the associated {@link org.ehcache.Cache}
+   * @return the {@link org.ehcache.spi.loader.CacheLoader} to be used by the {@link org.ehcache.Cache} or null if none
    */
   <K, V> CacheLoader<? super K, ? extends V> createLoader(String alias, CacheConfiguration<K, V> cacheConfiguration);
 
   /**
    * Invoked by {@link org.ehcache.CacheManager} when a {@link org.ehcache.Cache} is being removed from it.
-   * @param cacheLoader
+   * @param cacheLoader the {@link org.ehcache.spi.loader.CacheLoader} that was initially associated with
+   *                    the {@link org.ehcache.Cache} being removed
    */
   void releaseCacheLoader(CacheLoader<?, ?> cacheLoader);
 
