f373747f5449b5ffd1dc1925d84eaf97999ca12b
Mangalick <gam@mcgam02.attlocal.net>
2014-11-06 13:48:30 -0800

    #86, fixed code sytle, change method name, remove dedundant methods

--- core-spi-test/src/main/java/org/ehcache/internal/store/StoreBulkComputeIfAbsentTest.java
+++ core-spi-test/src/main/java/org/ehcache/internal/store/StoreBulkComputeIfAbsentTest.java
@@ -42,242 +42,162 @@ import static org.hamcrest.Matchers.nullValue;
 
 public class StoreBulkComputeIfAbsentTest<K, V> extends SPIStoreTester<K, V> {
 
-    public StoreBulkComputeIfAbsentTest(final StoreFactory<K, V> factory) {
-        super(factory);
-    }
+  public StoreBulkComputeIfAbsentTest(final StoreFactory<K, V> factory) {
+    super(factory);
+  }
 
-    @SPITest
-    public void testBulkComputeIfAbsentHappyPath() throws Exception {
-        final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
-                factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
-        final K k1 = factory.createKey(1L);
-        final V v1 = factory.createValue(1L);
-        final K k2 = factory.createKey(2L);
-        final V v2 = factory.createValue(2L);
-        final K k3 = factory.createKey(3L);
-        final V v3 = factory.createValue(3L);
-        final V v10 = factory.createValue(10L);
-        final V v20 = factory.createValue(20L);
-        final V v30 = factory.createValue(30L);
-        Map<K, V> map = new HashMap<K, V>() {{
-            put(k3, v3);
+  @SPITest
+  public void testBulkComputeIfReturnAllPassedEntries() throws Exception {
+    final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
+        factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
+    final K k1 = factory.createKey(1L);
+    final V v1 = factory.createValue(1L);
+    final K k2 = factory.createKey(2L);
+    final V v2 = factory.createValue(2L);
+    final K k3 = factory.createKey(3L);
+    final V v3 = factory.createValue(3L);
+    final V v10 = factory.createValue(10L);
+    final V v20 = factory.createValue(20L);
+    final V v30 = factory.createValue(30L);
+    Map<K, V> map = new HashMap<K, V>() {{
+      put(k3, v3);
+      put(k2, v2);
+      put(k1, v1);
+    }};
+    kvStore.put(k2, v2);
+    kvStore.put(k1, v1);
+    try {
+      Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
+        @Override
+        public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
+          return new HashMap<K, V>() {{
+            put(k3, v30);
             put(k2, v2);
-            put(k1, v1);
-        }};
-        kvStore.put(k2, v2);
-        kvStore.put(k1, v1);
-        try {
-            Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
-                @Override
-                public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
-                    return new HashMap<K, V>() {{
-                        put(k3, v30);
-                        put(k2, v2);
-                        put(k1, v10);
-                    }}.entrySet();
-                }
-            });
-            assertThat(result.get(k3).value(), is(v30));
-            assertThat(result.get(k2).value(), is(v2));
-            assertThat(result.get(k1).value(), is(v1));
-            assertThat(kvStore.get(k3).value(), is(v30));
-            assertThat(kvStore.get(k2).value(), is(v2));
-            assertThat(kvStore.get(k1).value(), is(v1));
-        } catch (CacheAccessException e) {
-            System.err.println("Warning, an exception is thrown due to the SPI test");
-            e.printStackTrace();
+            put(k1, v10);
+          }}.entrySet();
         }
+      });
+      assertThat(result.get(k3).value(), is(v30));
+      assertThat(result.get(k2).value(), is(v2));
+      assertThat(result.get(k1).value(), is(v1));
+      assertThat(kvStore.get(k3).value(), is(v30));
+      assertThat(kvStore.get(k2).value(), is(v2));
+      assertThat(kvStore.get(k1).value(), is(v1));
+    } catch (CacheAccessException e) {
+      System.err.println("Warning, an exception is thrown due to the SPI test");
+      e.printStackTrace();
     }
+  }
 
-    @SPITest
-    public void testBulkComputeIfAbsentFunctionReturnsNull() throws Exception {
-        final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
-                factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
-        final K k1 = factory.createKey(1L);
-        final V v1 = factory.createValue(1L);
-        final K k2 = factory.createKey(2L);
-        final V v2 = factory.createValue(2L);
-        final K k3 = factory.createKey(3L);
-        final V v3 = factory.createValue(3L);
+  @SPITest
+  public void testBulkComputeIfAbsentFunctionReturnsNull() throws Exception {
+    final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
+        factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
+    final K k1 = factory.createKey(1L);
+    final V v1 = factory.createValue(1L);
+    final K k2 = factory.createKey(2L);
+    final V v2 = factory.createValue(2L);
+    final K k3 = factory.createKey(3L);
+    final V v3 = factory.createValue(3L);
 
-        Map<K, V> map = new HashMap<K, V>() {{
-            put(k3, v3);
-            put(k2, v2);
-            put(k1, v1);
-        }};
-        kvStore.put(k2, v2);
-        kvStore.put(k1, v1);
-        try {
-            Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
-                @Override
-                public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
-                    return null;
-                }
-            });
-            assertThat(result.get(k2).value(), is(v2));
-            assertThat(result.get(k1).value(), is(v1));
-            assertThat(kvStore.get(k3), is(nullValue()));
-            assertThat(kvStore.get(k2).value(), is(v2));
-            assertThat(kvStore.get(k1).value(), is(v1));
-        } catch (CacheAccessException e) {
-            System.err.println("Warning, an exception is thrown due to the SPI test");
-            e.printStackTrace();
+    Map<K, V> map = new HashMap<K, V>() {{
+      put(k3, v3);
+      put(k2, v2);
+      put(k1, v1);
+    }};
+    kvStore.put(k2, v2);
+    kvStore.put(k1, v1);
+    try {
+      Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
+        @Override
+        public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
+          return null;
         }
+      });
+      assertThat(result.get(k2).value(), is(v2));
+      assertThat(result.get(k1).value(), is(v1));
+      assertThat(kvStore.get(k3), is(nullValue()));
+      assertThat(kvStore.get(k2).value(), is(v2));
+      assertThat(kvStore.get(k1).value(), is(v1));
+    } catch (CacheAccessException e) {
+      System.err.println("Warning, an exception is thrown due to the SPI test");
+      e.printStackTrace();
     }
+  }
 
-    @SPITest
-    public void testBulkComputeIfAbsentFunctionReturnsLessEntries() throws Exception {
-        final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
-                factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
-        final K k1 = factory.createKey(1L);
-        final V v1 = factory.createValue(1L);
-        final K k2 = factory.createKey(2L);
-        final V v2 = factory.createValue(2L);
-        final K k3 = factory.createKey(3L);
-        final V v3 = factory.createValue(3L);
-        final V v10 = factory.createValue(10L);
-        final V v20 = factory.createValue(20L);
-        final V v30 = factory.createValue(30L);
-        Map<K, V> map = new HashMap<K, V>() {{
-            put(k3, v3);
-            put(k2, v2);
-            put(k1, v1);
-        }};
-        try {
-            Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
-                @Override
-                public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
-                    return new HashMap<K, V>() {{
-                        put(k2, v20);
-                        put(k1, v10);
-                    }}.entrySet();
-                }
-            });
-            assertThat(result.get(k2).value(), is(v20));
-            assertThat(result.get(k1).value(), is(v10));
-            assertThat(kvStore.get(k3), is(nullValue()));
-            assertThat(kvStore.get(k2).value(), is(v20));
-            assertThat(kvStore.get(k1).value(), is(v10));
-        } catch (CacheAccessException e) {
-            System.err.println("Warning, an exception is thrown due to the SPI test");
-            e.printStackTrace();
+  @SPITest
+  public void testBulkComputeIfAbsentFunctionReturnsLessEntries() throws Exception {
+    final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
+        factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
+    final K k1 = factory.createKey(1L);
+    final V v1 = factory.createValue(1L);
+    final K k2 = factory.createKey(2L);
+    final V v2 = factory.createValue(2L);
+    final K k3 = factory.createKey(3L);
+    final V v3 = factory.createValue(3L);
+    final V v10 = factory.createValue(10L);
+    final V v20 = factory.createValue(20L);
+    final V v30 = factory.createValue(30L);
+    Map<K, V> map = new HashMap<K, V>() {{
+      put(k3, v3);
+      put(k2, v2);
+      put(k1, v1);
+    }};
+    try {
+      Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
+        @Override
+        public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
+          return new HashMap<K, V>() {{
+            put(k2, v20);
+            put(k1, v10);
+          }}.entrySet();
         }
+      });
+      assertThat(result.get(k2).value(), is(v20));
+      assertThat(result.get(k1).value(), is(v10));
+      assertThat(kvStore.get(k3), is(nullValue()));
+      assertThat(kvStore.get(k2).value(), is(v20));
+      assertThat(kvStore.get(k1).value(), is(v10));
+    } catch (CacheAccessException e) {
+      System.err.println("Warning, an exception is thrown due to the SPI test");
+      e.printStackTrace();
     }
+  }
 
-    @SPITest
-    public void testBulkComputeIfAbsentFunctionReturnsMoreEntries() throws Exception {
-        final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
-                factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
-        final K k1 = factory.createKey(1L);
-        final V v1 = factory.createValue(1L);
-        final K k2 = factory.createKey(2L);
-        final V v2 = factory.createValue(2L);
-        final K k3 = factory.createKey(3L);
-        final V v3 = factory.createValue(3L);
-        final V v10 = factory.createValue(10L);
-        final V v20 = factory.createValue(20L);
-        final V v30 = factory.createValue(30L);
-        Map<K, V> map = new HashMap<K, V>() {{
-            put(k2, v2);
-            put(k1, v1);
-        }};
-        try {
-            Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
-                @Override
-                public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
-                    return new HashMap<K, V>() {{
-                        put(k3, v30);
-                        put(k2, v20);
-                        put(k1, v10);
-                    }}.entrySet();
-                }
-            });
-            assertThat(result.get(k3), is(nullValue()));
-            assertThat(result.get(k2).value(), is(v20));
-            assertThat(result.get(k1).value(), is(v10));
-            assertThat(kvStore.get(k3), is(nullValue()));
-            assertThat(kvStore.get(k2).value(), is(v20));
-            assertThat(kvStore.get(k1).value(), is(v10));
-        } catch (CacheAccessException e) {
-            System.err.println("Warning, an exception is thrown due to the SPI test");
-            e.printStackTrace();
+  @SPITest
+  public void testBulkComputeFunctionReturnsNullValues() throws Exception {
+    final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
+        factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
+    final K k1 = factory.createKey(1L);
+    final V v1 = factory.createValue(1L);
+    final K k2 = factory.createKey(2L);
+    final V v2 = factory.createValue(2L);
+    final K k3 = factory.createKey(3L);
+    final V v3 = factory.createValue(3L);
+    Map<K, V> map = new HashMap<K, V>() {{
+      put(k3, v3);
+      put(k2, v2);
+      put(k1, v1);
+    }};
+    try {
+      Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
+        @Override
+        public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
+          return new HashMap<K, V>() {{
+            put(k3, null);
+            put(k2, null);
+          }}.entrySet();
         }
+      });
+      assertThat(result.get(k3), is(nullValue()));
+      assertThat(result.get(k2), is(nullValue()));
+      assertThat(result.get(k1), is(nullValue()));
+      assertThat(kvStore.get(k3), is(nullValue()));
+      assertThat(kvStore.get(k2), is(nullValue()));
+      assertThat(kvStore.get(k1), is(nullValue()));
+    } catch (CacheAccessException e) {
+      System.err.println("Warning, an exception is thrown due to the SPI test");
+      e.printStackTrace();
     }
-
-    @SPITest
-    public void testBulkComputeFunctionReturnsNullValues() throws Exception {
-        final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
-                factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
-        final K k1 = factory.createKey(1L);
-        final V v1 = factory.createValue(1L);
-        final K k2 = factory.createKey(2L);
-        final V v2 = factory.createValue(2L);
-        final K k3 = factory.createKey(3L);
-        final V v3 = factory.createValue(3L);
-        Map<K, V> map = new HashMap<K, V>() {{
-            put(k3, v3);
-            put(k2, v2);
-            put(k1, v1);
-        }};
-        try {
-            Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
-                @Override
-                public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
-                    return new HashMap<K, V>() {{
-                        put(k3, null);
-                        put(k2, null);
-                    }}.entrySet();
-                }
-            });
-            assertThat(result.get(k3), is(nullValue()));
-            assertThat(result.get(k2), is(nullValue()));
-            assertThat(result.get(k1), is(nullValue()));
-            assertThat(kvStore.get(k3), is(nullValue()));
-            assertThat(kvStore.get(k2), is(nullValue()));
-            assertThat(kvStore.get(k1), is(nullValue()));
-        } catch (CacheAccessException e) {
-            System.err.println("Warning, an exception is thrown due to the SPI test");
-            e.printStackTrace();
-        }
-    }
-
-    @SPITest
-    public void testBulkComputeFunctionReturnsDifferentKeys() throws Exception {
-        final Store<K, V> kvStore = factory.newStore(new StoreConfigurationImpl<K, V>(factory.getKeyType(),
-                factory.getValueType(), null, Predicates.<Cache.Entry<K, V>>all(), null, ClassLoader.getSystemClassLoader()));
-        final K k1 = factory.createKey(1L);
-        final V v1 = factory.createValue(1L);
-        final K k2 = factory.createKey(2L);
-        final V v2 = factory.createValue(2L);
-        final K k3 = factory.createKey(3L);
-        final V v3 = factory.createValue(3L);
-        final K k4 = factory.createKey(4L);
-        final V v4 = factory.createValue(4L);
-        Map<K, V> map = new HashMap<K, V>() {{
-            put(k4, v4);
-            put(k3, v3);
-        }};
-        try {
-            Map<K, Store.ValueHolder<V>> result = kvStore.bulkComputeIfAbsent(map.keySet(), new Function<Iterable<? extends K>, Iterable<? extends Map.Entry<? extends K, ? extends V>>>() {
-                @Override
-                public Iterable<? extends Map.Entry<? extends K, ? extends V>> apply(Iterable<? extends K> ks) {
-                    return new HashMap<K, V>() {{
-                        put(k2, v2);
-                        put(k1, v1);
-                    }}.entrySet();
-                }
-            });
-            assertThat(result.get(k4), is(nullValue()));
-            assertThat(result.get(k3), is(nullValue()));
-            assertThat(result.get(k2), is(nullValue()));
-            assertThat(result.get(k1), is(nullValue()));
-            assertThat(kvStore.get(k4), is(nullValue()));
-            assertThat(kvStore.get(k3), is(nullValue()));
-            assertThat(kvStore.get(k2), is(nullValue()));
-            assertThat(kvStore.get(k1), is(nullValue()));
-        } catch (CacheAccessException e) {
-            System.err.println("Warning, an exception is thrown due to the SPI test");
-            e.printStackTrace();
-        }
-    }
+  }
 }
