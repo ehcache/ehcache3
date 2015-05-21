c54f88b54563d1b46c0dd82843f9378c777e16a7
Louis Jacomet <ljacomet@gmail.com>
2015-05-21 15:53:07 +0200

    Issue #394 Enforce no static imports in test classes in package org.ehcache.docs Exceptions made for org.hamcrest.Matchers and org.junit.Assert

--- 107/src/test/java/org/ehcache/docs/EhCache107ConfigurationIntegrationDocTest.java
+++ 107/src/test/java/org/ehcache/docs/EhCache107ConfigurationIntegrationDocTest.java
@@ -41,11 +41,11 @@ import javax.cache.expiry.Duration;
 import javax.cache.expiry.ExpiryPolicy;
 import javax.cache.spi.CachingProvider;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 /**
  * This class uses unit test assertions but serves mostly as the live code repository for Asciidoctor documentation.
