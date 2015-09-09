7015398dc4e6f9e3b3d997b8eca49a66ccdfe15d
Alex Snaps <alex.snaps@gmail.com>
2015-09-09 15:25:12 -0400

    Issue #523 Eh107Cache pass

--- 107/src/main/java/org/ehcache/jsr107/Eh107Cache.java
+++ 107/src/main/java/org/ehcache/jsr107/Eh107Cache.java
@@ -15,6 +15,19 @@
  */
 package org.ehcache.jsr107;
 
+import org.ehcache.Ehcache;
+import org.ehcache.EhcacheHackAccessor;
+import org.ehcache.Status;
+import org.ehcache.UserManagedCache;
+import org.ehcache.event.EventFiring;
+import org.ehcache.event.EventOrdering;
+import org.ehcache.function.BiFunction;
+import org.ehcache.function.Function;
+import org.ehcache.function.NullaryFunction;
+import org.ehcache.jsr107.EventListenerAdaptors.EventListenerAdaptor;
+import org.ehcache.management.ManagementRegistry;
+import org.ehcache.spi.loaderwriter.CacheLoaderWriter;
+
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,19 +46,6 @@ import javax.cache.processor.EntryProcessor;
 import javax.cache.processor.EntryProcessorException;
 import javax.cache.processor.EntryProcessorResult;
 
-import org.ehcache.Ehcache;
-import org.ehcache.EhcacheHackAccessor;
-import org.ehcache.Status;
-import org.ehcache.UserManagedCache;
-import org.ehcache.event.EventFiring;
-import org.ehcache.event.EventOrdering;
-import org.ehcache.function.BiFunction;
-import org.ehcache.function.Function;
-import org.ehcache.function.NullaryFunction;
-import org.ehcache.jsr107.EventListenerAdaptors.EventListenerAdaptor;
-import org.ehcache.management.ManagementRegistry;
-import org.ehcache.spi.loaderwriter.CacheLoaderWriter;
-
 /**
  * @author teck
  */
@@ -55,7 +55,7 @@ class Eh107Cache<K, V> implements Cache<K, V> {
   private final org.ehcache.Jsr107Cache<K, V> jsr107Cache;
   private final Eh107CacheManager cacheManager;
   private final String name;
-  private final AtomicBoolean closed = new AtomicBoolean();
+  private final AtomicBoolean hypotheticallyClosed = new AtomicBoolean();
   private final CacheResources<K, V> cacheResources;
   private final Eh107CacheMXBean managementBean;
   private final Eh107CacheStatisticsMXBean statisticsBean;
@@ -438,7 +438,7 @@ class Eh107Cache<K, V> implements Cache<K, V> {
 
   @Override
   public boolean isClosed() {
-    return closed.get() || ((UserManagedCache)ehCache).getStatus() != Status.AVAILABLE;
+    return syncedIsClose();
   }
 
   void closeInternal(MultiCacheException closeException) {
@@ -446,7 +446,7 @@ class Eh107Cache<K, V> implements Cache<K, V> {
   }
 
   private void closeInternal(boolean destroy, MultiCacheException closeException) {
-    if (closed.compareAndSet(false, true)) {
+    if (hypotheticallyClosed.compareAndSet(false, true)) {
       if (destroy) {
         try {
           clear(false);
@@ -457,6 +457,13 @@ class Eh107Cache<K, V> implements Cache<K, V> {
 
       cacheResources.closeResources(closeException);
     }
+  }
+
+  private boolean syncedIsClose() {
+    if (((UserManagedCache)ehCache).getStatus() == Status.UNINITIALIZED && !hypotheticallyClosed.get()) {
+      close();
+    }
+    return hypotheticallyClosed.get();
   }
 
   void destroy(MultiCacheException destroyException) {
@@ -521,7 +528,7 @@ class Eh107Cache<K, V> implements Cache<K, V> {
   }
 
   private void checkClosed() {
-    if (closed.get()) {
+    if (syncedIsClose()) {
       throw new IllegalStateException("Cache[" + name + "] is closed");
     }
   }
