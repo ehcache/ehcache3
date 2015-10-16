477d3a77e174554e746c6ff1e25b5b4d8f3d1808
Louis Jacomet <ljacomet@gmail.com>
2015-10-16 10:22:58 +0200

    Issue #614 Add OSGi entries to manifest
    First pass: Basic Ehcache works

---
+++ osgi-test/src/test/java/org/ehcache/osgi/SimpleOsgiTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright Terracotta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ehcache.osgi;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.CacheManagerBuilder;
+import org.ehcache.config.copy.CopierConfiguration;
+import org.ehcache.config.copy.DefaultCopierConfiguration;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.internal.copy.ReadWriteCopier;
+import org.ehcache.internal.copy.SerializingCopier;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+
+import java.io.Serializable;
+
+import static org.ehcache.config.CacheConfigurationBuilder.newCacheConfigurationBuilder;
+import static org.ehcache.config.ResourcePoolsBuilder.newResourcePoolsBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+/**
+ * SimpleOsgiTest
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class SimpleOsgiTest {
+
+  @Configuration
+  public Option[] config() {
+    return options(
+        mavenBundle("org.slf4j", "slf4j-api", "1.7.7"),
+        mavenBundle("org.slf4j", "slf4j-simple", "1.7.7").noStart(),
+        mavenBundle("org.ehcache", "ehcache", "3.0.0-SNAPSHOT"),
+        junitBundles()
+    );
+  }
+
+  @Test
+  public void testEhcache3AsBundle() {
+    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+        .withCache("myCache", newCacheConfigurationBuilder().withResourcePools(newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES))
+            .buildConfig(Long.class, String.class))
+        .build(true);
+
+    Cache<Long, String> myCache = cacheManager.getCache("myCache", Long.class, String.class);
+
+    myCache.put(42L, "DaAnswer!");
+    assertEquals("DaAnswer!", myCache.get(42L));
+  }
+
+  @Test
+  public void testEhcache3WithSerializationAndClientClass() {
+    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+        .withCache("myCache", newCacheConfigurationBuilder().withResourcePools(newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES))
+            .add(new DefaultCopierConfiguration<Person>((Class) SerializingCopier.class, CopierConfiguration.Type.VALUE))
+            .withClassLoader(getClass().getClassLoader())
+            .buildConfig(Long.class, Person.class))
+        .build(true);
+
+    Cache<Long, Person> myCache = cacheManager.getCache("myCache", Long.class, Person.class);
+
+    myCache.put(42L, new Person("Arthur"));
+    assertTrue(myCache.get(42L) instanceof Person);
+    System.out.println(myCache.get(42L));
+  }
+
+  @Test
+  public void testCustomCopier() {
+    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+        .withCache("myCache", newCacheConfigurationBuilder().withResourcePools(newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES))
+            .add(new DefaultCopierConfiguration<String>(StringCopier.class, CopierConfiguration.Type.VALUE))
+            .withClassLoader(Person.class.getClassLoader())
+            .buildConfig(Long.class, String.class))
+        .build(true);
+
+    Cache<Long, String> cache = cacheManager.getCache("myCache", Long.class, String.class);
+
+    cache.put(42L, "What's the question again?");
+    cache.get(42L);
+  }
+
+  public static class StringCopier extends ReadWriteCopier<String> {
+
+    @Override
+    public String copy(String obj) {
+      return new String(obj);
+    }
+  }
+
+  private static class Person implements Serializable {
+
+    final String name;
+    Person(String name) {
+      this.name = name;
+    }
+
+  }
+}
