7484085e151a996cf0aca5ec506a48bc51eb3e71
Chris Dennis <chris.w.dennis@gmail.com>
2016-02-10 16:46:05 -0500

    Issue #738 Initial CLI Skeleton

--- clustered/ops-tool/src/main/java/org/ehcache/clustered/operations/Command.java
+++ clustered/ops-tool/src/main/java/org/ehcache/clustered/operations/Command.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.ehcache.clustered.operations;
 
-include "api", "spi-tester", "core", "core-spi-test", "impl", "management", "transactions", "107", "xml",
-        "clustered", "clustered:common", "clustered:client", "clustered:server", "clustered:integration-test", "clustered:dist", "clustered:ops-tool",
-        "integration-test", "dist", "osgi-test", "demos", "demos:00-NoCache", "demos:01-CacheAside", "docs"
+interface Command {
+  
+  int execute();
+}
