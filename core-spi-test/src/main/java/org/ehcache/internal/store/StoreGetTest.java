f7cf6b525bef9147ed2af5fd5f25b3c77e099dae
Louis Jacomet <ljacomet@gmail.com>
2016-03-16 22:15:29 +0100

    Issue #825 Add more SPITests
    * Tests for expiry on access and update

--- core-spi-test/src/main/java/org/ehcache/internal/store/StoreGetTest.java
+++ core-spi-test/src/main/java/org/ehcache/internal/store/StoreGetTest.java
@@ -18,6 +18,9 @@ package org.ehcache.internal.store;
 
 import org.ehcache.core.spi.cache.Store;
 import org.ehcache.exceptions.CacheAccessException;
+import org.ehcache.expiry.Duration;
+import org.ehcache.expiry.Expiry;
+import org.ehcache.internal.TestTimeSource;
 import org.ehcache.spi.test.After;
 import org.ehcache.spi.test.LegalSPITesterException;
 import org.ehcache.spi.test.SPITest;
@@ -125,7 +128,6 @@ public class StoreGetTest<K, V> extends SPIStoreTester<K, V> {
   }
 
   @SPITest
-  // @Ignore(reason = "Failing test to fix")
   @SuppressWarnings({ "unchecked", "rawtypes" })
   public void wrongKeyTypeThrowsException()
       throws IllegalAccessException, InstantiationException, LegalSPITesterException {
@@ -140,6 +142,37 @@ public class StoreGetTest<K, V> extends SPIStoreTester<K, V> {
       throw new AssertionError("Expected ClassCastException because the key is of the wrong type");
     } catch (ClassCastException e) {
       // expected
+    } catch (CacheAccessException e) {
+      throw new LegalSPITesterException("Warning, an exception is thrown due to the SPI test");
+    }
+  }
+
+  @SPITest
+  public void testGetExpiresOnAccess() throws LegalSPITesterException {
+    TestTimeSource timeSource = new TestTimeSource(10043L);
+    kvStore = factory.newStoreWithExpiry(new Expiry<K, V>() {
+      @Override
+      public Duration getExpiryForCreation(K key, V value) {
+        return Duration.FOREVER;
+      }
+
+      @Override
+      public Duration getExpiryForAccess(K key, V value) {
+        return Duration.ZERO;
+      }
+
+      @Override
+      public Duration getExpiryForUpdate(K key, V oldValue, V newValue) {
+        return Duration.FOREVER;
+      }
+    }, timeSource);
+
+    K key = factory.createKey(250928L);
+    V value = factory.createValue(2059820L);
+
+    try {
+      kvStore.put(key, value);
+      assertThat(kvStore.get(key).value(), is(value));
     } catch (CacheAccessException e) {
       throw new LegalSPITesterException("Warning, an exception is thrown due to the SPI test");
     }
