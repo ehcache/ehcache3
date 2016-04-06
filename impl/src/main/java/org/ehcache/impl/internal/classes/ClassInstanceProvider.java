d84dc4cdf313c99ea74a4478996902c594cc2ac1
Abhilash <ab.co.er@gmail.com>
2016-04-07 02:17:27 +0530

    Do not close user provided instances #837

--- impl/src/main/java/org/ehcache/impl/internal/classes/ClassInstanceProvider.java
+++ impl/src/main/java/org/ehcache/impl/internal/classes/ClassInstanceProvider.java
@@ -26,9 +26,11 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.ehcache.impl.internal.classes.commonslang.reflect.ConstructorUtils.invokeConstructor;
@@ -49,6 +51,7 @@ public class ClassInstanceProvider<K, T> {
    * Instances provided by this provider vs their counts.
    */
   protected final ConcurrentWeakIdentityHashMap<T, AtomicInteger> providedVsCount = new ConcurrentWeakIdentityHashMap<T, AtomicInteger>();
+  protected final Set<T> instantiated = new HashSet<T>();
 
   private final Class<? extends ClassInstanceConfiguration<T>> cacheLevelConfig;
   private final boolean uniqueClassLevelConfig;
@@ -109,6 +112,7 @@ public class ClassInstanceProvider<K, T> {
     } else {
       try {
         instance = invokeConstructor(config.getClazz(), config.getArguments());
+        instantiated.add(instance);
       } catch (InstantiationException e) {
         throw new RuntimeException(e);
       } catch (IllegalAccessException e) {
@@ -138,9 +142,10 @@ public class ClassInstanceProvider<K, T> {
       throw new IllegalArgumentException("Given instance of " + instance.getClass().getName() + " is not managed by this provider");
     }
 
-    if(instance instanceof Closeable) {
+    if(instantiated.contains(instance) && instance instanceof Closeable) {
       ((Closeable)instance).close();
     }
+    instantiated.remove(instance);
   }
 
   public void start(ServiceProvider<Service> serviceProvider) {
--- impl/src/test/java/org/ehcache/impl/internal/classes/ClassInstanceProviderTest.java
+++ impl/src/test/java/org/ehcache/impl/internal/classes/ClassInstanceProviderTest.java
@@ -31,6 +31,7 @@ import static org.hamcrest.Matchers.sameInstance;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -102,6 +103,7 @@ public class ClassInstanceProviderTest {
     ClassInstanceProvider<String, Closeable> classInstanceProvider = new ClassInstanceProvider<String, Closeable>(null, null);
     Closeable closeable = mock(Closeable.class);
     classInstanceProvider.providedVsCount.put(closeable, new AtomicInteger(1));
+    classInstanceProvider.instantiated.add(closeable);
 
     classInstanceProvider.releaseInstance(closeable);
     verify(closeable).close();
@@ -113,6 +115,7 @@ public class ClassInstanceProviderTest {
     Closeable closeable = mock(Closeable.class);
     doThrow(IOException.class).when(closeable).close();
     classInstanceProvider.providedVsCount.put(closeable, new AtomicInteger(1));
+    classInstanceProvider.instantiated.add(closeable);
 
     classInstanceProvider.releaseInstance(closeable);
   }
@@ -142,6 +145,41 @@ public class ClassInstanceProviderTest {
     newService = classInstanceProvider.newInstance("test stuff", config);
     assertThat(newService, sameInstance(service));
     assertThat(classInstanceProvider.providedVsCount.get(service).get(), is(2));
+  }
+
+  @Test
+  public void testInstancesNotCreatedByProviderDoesNotClose() throws IOException {
+    ClassInstanceProvider<String, TestCloaseableService> classInstanceProvider = new ClassInstanceProvider<String, TestCloaseableService>(null, (Class)ClassInstanceConfiguration.class);
+
+    TestCloaseableService service = mock(TestCloaseableService.class);
+    TestCloaseableServiceConfig config = new TestCloaseableServiceConfig(service);
+
+    TestCloaseableService newService = classInstanceProvider.newInstance("testClose", config);
+    assertThat(newService, sameInstance(service));
+    classInstanceProvider.releaseInstance(newService);
+    verify(service, times(0)).close();
+
+  }
+
+
+  public static abstract class TestCloaseableService implements Service, Closeable {
+
+  }
+
+  public static class TestCloaseableServiceConfig extends ClassInstanceConfiguration<TestCloaseableService> implements ServiceConfiguration<TestCloaseableService> {
+
+    public TestCloaseableServiceConfig() {
+      super(TestCloaseableService.class);
+    }
+
+    public TestCloaseableServiceConfig(TestCloaseableService testCloaseableService) {
+      super(testCloaseableService);
+    }
+
+    @Override
+    public Class<TestCloaseableService> getServiceType() {
+      return TestCloaseableService.class;
+    }
   }
 
   public static class TestService implements Service {
