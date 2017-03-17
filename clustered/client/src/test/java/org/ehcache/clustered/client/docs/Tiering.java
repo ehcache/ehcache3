881948f9fda76b4a9145f4a511a33909c16cd2fc
Henri Tremblay <henri.tremblay@gmail.com>
2017-03-17 11:17:34 -0400

    Tiering documentation (close #1905)

--- clustered/client/src/test/java/org/ehcache/clustered/client/docs/Tiering.java
+++ clustered/client/src/test/java/org/ehcache/clustered/client/docs/Tiering.java
@@ -16,16 +16,42 @@
 
 package org.ehcache.clustered.client.docs;
 
+import java.net.URI;
+
+import org.ehcache.PersistentCacheManager;
 import org.ehcache.clustered.client.config.builders.ClusteredResourcePoolBuilder;
+import org.ehcache.clustered.client.internal.UnitTestConnectionService;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.ehcache.clustered.client.config.builders.ClusteringServiceConfigurationBuilder.cluster;
 
 /**
  * Tiering
  */
 public class Tiering {
+
+  private static final URI CLUSTER_URI = URI.create("terracotta://example.com:9540/my-application");
+
+  @Before
+  public void definePassthroughServer() throws Exception {
+    UnitTestConnectionService.add(CLUSTER_URI,
+      new UnitTestConnectionService.PassthroughServerBuilder()
+        .resource("primary-server-resource", 64, MemoryUnit.MB)
+        .resource("secondary-server-resource", 64, MemoryUnit.MB)
+        .build());
+  }
+
+  @After
+  public void removePassthroughServer() throws Exception {
+    UnitTestConnectionService.remove(CLUSTER_URI);
+  }
 
   @Test
   public void testSingleTier() {
@@ -34,5 +60,23 @@ public class Tiering {
       ResourcePoolsBuilder.newResourcePoolsBuilder()
         .with(ClusteredResourcePoolBuilder.clusteredDedicated(2, MemoryUnit.GB))); // <2>
     // end::clusteredOnly[]
+  }
+
+  @Test
+  public void threeTiersCacheManager() throws Exception {
+    // tag::threeTiersCacheManager[]
+    PersistentCacheManager persistentCacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+      .with(cluster(CLUSTER_URI).autoCreate()) // <1>
+      .withCache("threeTierCache",
+        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+          ResourcePoolsBuilder.newResourcePoolsBuilder()
+            .heap(10, EntryUnit.ENTRIES) // <2>
+            .offheap(1, MemoryUnit.MB) // <3>
+            .with(ClusteredResourcePoolBuilder.clusteredDedicated("primary-server-resource", 2, MemoryUnit.MB)) // <4>
+        )
+      ).build(true);
+    // end::threeTiersCacheManager[]
+
+    persistentCacheManager.close();
   }
 }
--- impl/src/test/java/org/ehcache/docs/Tiering.java
+++ impl/src/test/java/org/ehcache/docs/Tiering.java
@@ -16,15 +16,31 @@
 
 package org.ehcache.docs;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.PersistentCacheManager;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.ResourcePools;
+import org.ehcache.config.ResourceType;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheEventListenerConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.docs.plugs.ListenerObject;
+import org.ehcache.event.EventType;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.IOException;
+import static org.ehcache.config.builders.CacheConfigurationBuilder.newCacheConfigurationBuilder;
+import static org.ehcache.config.builders.CacheManagerBuilder.newCacheManagerBuilder;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tiering
@@ -35,6 +51,20 @@ public class Tiering {
   public TemporaryFolder folder = new TemporaryFolder();
 
   @Test
+  public void tierSizing() {
+    // tag::heap[]
+    ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10, EntryUnit.ENTRIES); // <1>
+    // or
+    ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10); // <2>
+    // or
+    ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10, MemoryUnit.MB); // <3>
+    // end::heap[]
+    // tag::offheap[]
+    ResourcePoolsBuilder.newResourcePoolsBuilder().offheap(10, MemoryUnit.MB); // <1>
+    // end::offheap[]
+  }
+
+  @Test
   public void testSingleTier() {
     // tag::offheapOnly[]
     CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class, // <1>
@@ -43,17 +73,124 @@ public class Tiering {
   }
 
   @Test
+  public void threeTiersCacheManager() throws Exception {
+    // tag::threeTiersCacheManager[]
+    PersistentCacheManager persistentCacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+      .with(CacheManagerBuilder.persistence(new File(getStoragePath(), "myData")))
+      .withCache("threeTieredCache",
+        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+          ResourcePoolsBuilder.newResourcePoolsBuilder()
+            .heap(10, EntryUnit.ENTRIES)
+            .offheap(1, MemoryUnit.MB)
+            .disk(20, MemoryUnit.MB, true)
+        )
+      ).build(true);
+    // end::threeTiersCacheManager[]
+
+    persistentCacheManager.close();
+  }
+
+  @Test
+  public void persistentCacheManager() throws Exception {
+    // tag::persistentCacheManager[]
+    PersistentCacheManager persistentCacheManager = CacheManagerBuilder.newCacheManagerBuilder() // <2>
+      .with(CacheManagerBuilder.persistence(new File(getStoragePath(), "myData"))) // <1>
+      .withCache("persistent-cache", CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+        ResourcePoolsBuilder.newResourcePoolsBuilder().disk(10, MemoryUnit.MB, true)) // <3>
+      )
+      .build(true);
+
+    persistentCacheManager.close();
+    // end::persistentCacheManager[]
+  }
+
+  @Test
+  public void updateResourcesAtRuntime() throws InterruptedException {
+    ListenerObject listener = new ListenerObject();
+    CacheEventListenerConfigurationBuilder cacheEventListenerConfiguration = CacheEventListenerConfigurationBuilder
+      .newEventListenerConfiguration(listener, EventType.EVICTED).unordered().synchronous();
+
+    CacheConfiguration<Long, String> cacheConfiguration = CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+      ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10L, EntryUnit.ENTRIES))
+      .add(cacheEventListenerConfiguration)
+      .build();
+
+    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().withCache("cache", cacheConfiguration)
+      .build(true);
+
+    Cache<Long, String> cache = cacheManager.getCache("cache", Long.class, String.class);
+    for(long i = 0; i < 20; i++ ){
+      cache.put(i, "Hello World");
+    }
+    assertThat(listener.evicted(), is(10));
+
+    cache.clear();
+    listener.resetEvictionCount();
+
+    // tag::updateResourcesAtRuntime[]
+    ResourcePools pools = ResourcePoolsBuilder.newResourcePoolsBuilder().heap(20L, EntryUnit.ENTRIES).build(); // <1>
+    cache.getRuntimeConfiguration().updateResourcePools(pools); // <2>
+    assertThat(cache.getRuntimeConfiguration().getResourcePools()
+      .getPoolForResource(ResourceType.Core.HEAP).getSize(), is(20L));
+    // end::updateResourcesAtRuntime[]
+
+    for(long i = 0; i < 20; i++ ){
+      cache.put(i, "Hello World");
+    }
+    assertThat(listener.evicted(), is(0));
+
+    cacheManager.close();
+  }
+
+  @Test
   public void testPersistentDiskTier() throws Exception {
     // tag::diskPersistent[]
     CacheManagerBuilder.newCacheManagerBuilder()
-      .with(CacheManagerBuilder.persistence(getFilePath())) // <1>
+      .with(CacheManagerBuilder.persistence(getStoragePath())) // <1>
       .withCache("myCache",
         CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
           ResourcePoolsBuilder.newResourcePoolsBuilder().disk(1, MemoryUnit.GB, true))); // <2>
-    // end::diskPersistent[]
+    // end::diskPersistent[]f
   }
 
-  public String getFilePath() throws IOException {
+  @Test
+  public void testNotShared() {
+    // tag::notShared[]
+    ResourcePools pool = ResourcePoolsBuilder.newResourcePoolsBuilder().heap(10).build();
+
+    CacheManager cacheManager = newCacheManagerBuilder()
+      .withCache("test-cache1", newCacheConfigurationBuilder(Integer.class, String.class, pool))
+      .withCache("test-cache2", newCacheConfigurationBuilder(Integer.class, String.class, pool))
+      .build(true);
+    // end::notShared[]
+  }
+
+  @Test
+  public void byteSizedTieredCache() {
+    // tag::byteSizedTieredCache[]
+    CacheConfiguration<Long, String> usesConfiguredInCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+      ResourcePoolsBuilder.newResourcePoolsBuilder()
+        .heap(10, MemoryUnit.KB) // <1>
+        .offheap(10, MemoryUnit.MB)) // <2>
+      .withSizeOfMaxObjectGraph(1000)
+      .withSizeOfMaxObjectSize(1000, MemoryUnit.B) // <3>
+      .build();
+
+    CacheConfiguration<Long, String> usesDefaultSizeOfEngineConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+      ResourcePoolsBuilder.newResourcePoolsBuilder()
+        .heap(10, MemoryUnit.KB))
+      .build();
+
+    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+      .withDefaultSizeOfMaxObjectSize(500, MemoryUnit.B)
+      .withDefaultSizeOfMaxObjectGraph(2000) // <4>
+      .withCache("usesConfiguredInCache", usesConfiguredInCacheConfig)
+      .withCache("usesDefaultSizeOfEngine", usesDefaultSizeOfEngineConfig)
+      .build(true);
+    // end::byteSizedTieredCache[]
+  }
+
+  private String getStoragePath() throws IOException {
     return folder.newFolder().getAbsolutePath();
   }
 }
