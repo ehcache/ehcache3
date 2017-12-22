46d2a18a08028b616e9d32a13ca9c53ebd39e404
Henri Tremblay <henri.tremblay@gmail.com>
2017-12-22 11:03:41 -0500

    Resilience implementation moved out of internal (#2198)

--- core/src/main/java/org/ehcache/core/resilience/RecoveryCache.java
+++ core/src/main/java/org/ehcache/core/resilience/RecoveryCache.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.ehcache.core.internal.resilience;
+package org.ehcache.core.resilience;
 
 import org.ehcache.resilience.StoreAccessException;
 
--- core/src/main/java/org/ehcache/core/resilience/RobustResilienceStrategy.java
+++ core/src/main/java/org/ehcache/core/resilience/RobustResilienceStrategy.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.ehcache.core.internal.resilience;
+package org.ehcache.core.resilience;
 
 import java.util.HashMap;
 import java.util.Map;
