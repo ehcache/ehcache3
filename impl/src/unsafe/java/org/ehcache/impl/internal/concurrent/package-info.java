e58e11e3bc9946869f980f56318baaa8825b9736
Henri Tremblay <henri.tremblay@gmail.com>
2018-02-13 15:40:18 -0500

    Put classes using unsafe in a dedicated sourceSet to allow using -Werror on impl

--- impl/src/unsafe/java/org/ehcache/impl/internal/concurrent/package-info.java
+++ impl/src/unsafe/java/org/ehcache/impl/internal/concurrent/package-info.java
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-@edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 package org.ehcache.impl.internal.concurrent;
 
