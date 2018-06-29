f19bb56c26968492283da6285bc672b33440e930
mdh <MdMobasherul.Haque@softwareag.com>
2018-06-29 17:29:37 +0530

    Throwing exception for reverse translation of jsr cache elements.

--- 107/src/test/java/org/ehcache/jsr107/internal/Jsr107ServiceConfigurationParserTest.java
+++ 107/src/test/java/org/ehcache/jsr107/internal/Jsr107ServiceConfigurationParserTest.java
@@ -17,19 +17,18 @@ package org.ehcache.jsr107.internal;
 
 import org.ehcache.jsr107.config.ConfigurationElementState;
 import org.ehcache.jsr107.config.Jsr107Configuration;
+import org.ehcache.xml.exceptions.XmlConfigurationException;
 import org.junit.Test;
-import org.w3c.dom.Node;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.ehcache.xml.ConfigurationParserTestHelper.assertElement;
 
 /**
  * Jsr107ServiceConfigurationParserTest
  */
 public class Jsr107ServiceConfigurationParserTest {
-  @Test
+
+  @Test(expected = XmlConfigurationException.class)
   public void testTranslateServiceCreationConfiguration() {
     Jsr107ServiceConfigurationParser configTranslator = new Jsr107ServiceConfigurationParser();
 
@@ -41,33 +40,7 @@ public class Jsr107ServiceConfigurationParserTest {
       new Jsr107Configuration("tiny-template", templateMap, jsr107CompliantAtomics,
         ConfigurationElementState.ENABLED, ConfigurationElementState.DISABLED);
 
-    Node retElement = configTranslator.unparseServiceCreationConfiguration(serviceCreationConfiguration);
-    String inputString = "<jsr107:defaults " +
-                         "default-template = \"tiny-template\" enable-management = \"true\" " +
-                         "enable-statistics = \"false\" jsr-107-compliant-atomics = \"true\" " +
-                         "xmlns:jsr107 = \"http://www.ehcache.org/v3/jsr107\">" +
-                         "<jsr107:cache name= \"testCache\" template = \"simpleCacheTemplate\"/> " +
-                         "<jsr107:cache name = \"testCache1\" template = \"simpleCacheTemplate1\"/> " +
-                         "</jsr107:defaults>";
-    assertElement(inputString, retElement);
-  }
-
-  @Test
-  public void testTranslateServiceWithManagementStatisticsUnspecifiedAndNoCaches() {
-    Jsr107ServiceConfigurationParser configTranslator = new Jsr107ServiceConfigurationParser();
-    boolean jsr107CompliantAtomics = false;
-    Map<String, String> templateMap = new HashMap<>();
-    Jsr107Configuration serviceCreationConfiguration =
-      new Jsr107Configuration("tiny-template", templateMap, jsr107CompliantAtomics,
-        ConfigurationElementState.UNSPECIFIED, ConfigurationElementState.UNSPECIFIED);
-
-    Node retElement = configTranslator.unparseServiceCreationConfiguration(serviceCreationConfiguration);
-    String inputString = "<jsr107:defaults " +
-                         "default-template = \"tiny-template\" " +
-                         "jsr-107-compliant-atomics = \"false\" " +
-                         "xmlns:jsr107 = \"http://www.ehcache.org/v3/jsr107\">" +
-                         "</jsr107:defaults>";
-    assertElement(inputString, retElement);
+    configTranslator.unparseServiceCreationConfiguration(serviceCreationConfiguration);
   }
 
 }
