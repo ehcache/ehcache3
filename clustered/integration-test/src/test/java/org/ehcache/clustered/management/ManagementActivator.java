777e15aedebf1acaa6ee63e66730355cd2c5ea34
Mathieu Carbou <mathieu.carbou@gmail.com>
2018-09-12 09:59:47 -0400

    checkstyle fix

--- clustered/integration-test/src/test/java/org/ehcache/clustered/management/ManagementActivator.java
+++ clustered/integration-test/src/test/java/org/ehcache/clustered/management/ManagementActivator.java
@@ -1,6 +1,17 @@
 /*
- * Copyright (c) 2011-2018 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
- * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
+ * Copyright Terracotta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.ehcache.clustered.management;
 
