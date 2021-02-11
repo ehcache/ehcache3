3743a17a9324fd1a264e5618836f27e91f777d37
Reşat SABIQ <haqer@gmx.fr>
2021-02-11 12:23:08 +0600

    Parameterized JUnit test for issue #2876.

---
+++ core/src/test/java/org/ehcache/core/config/EhCacheProviderWithXMLOrJavaBasedConfig.java
@@ -0,0 +1,87 @@
+package com.adazes.contribution.ehcache.issue;
+
+import java.io.File;
+import java.net.URL;
+
+import org.ehcache.Cache;
+import org.ehcache.PersistentCacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.xml.XmlConfiguration;
+
+/**
+ * @author Reşat SABIQ
+ */
+public class EhCacheProviderWithXMLOrJavaBasedConfig {
+	private static final String PERSISTENCE_DIRECTORY = "/temp/ehcache";
+	private static final String CACHE_ALIAS = "bugReportCache";
+	private static final String XML_CONFIG_PATH = "/ehcache-dir-persistence-test.xml";
+
+	/**
+	 * For restoration of cache after process termination this has to be false so that Java API config is used.
+	 * Then, the following file is persisted when manager is closed, which allows to restart a cache with previous
+	 * mappings:
+	 * -rw-rw-r-- 1 resat resat	697 Fev 10 20:29 ehcache-disk-store.index
+	 * If the data is recovered, the following is logged:
+	 * Feb 10, 2021 8:41:15 PM org.ehcache.impl.internal.store.disk.OffHeapDiskStore recoverBackingMap
+		INFO: The index for data file ehcache-disk-store.data is more recent than the data file itself by 345ms :
+			this is harmless.
+	 */
+	protected static final boolean USE_XML_BASED_CONFIG = false;
+
+	private PersistentCacheManager ehCacheManager;
+	private Cache<String, String> cache;
+
+	static String getStoragePath() {
+		return System.getProperty("user.home") + PERSISTENCE_DIRECTORY;
+	}
+
+	static PersistentCacheManager initCacheManagerFromXMLConfig() {
+		URL myUrl = EhCacheProviderWithXMLOrJavaBasedConfig.class.getResource(XML_CONFIG_PATH);
+		XmlConfiguration xmlConfig = new XmlConfiguration(myUrl); 
+		return (PersistentCacheManager)CacheManagerBuilder.newCacheManager(xmlConfig);
+	}
+
+	static PersistentCacheManager initCacheManagerUsingJavaAPI() {
+		PersistentCacheManager persistentCacheManager = 
+			CacheManagerBuilder.newCacheManagerBuilder()
+				.with(CacheManagerBuilder.persistence(getStoragePath()+ File.separator))
+				.withCache(CACHE_ALIAS, CacheConfigurationBuilder
+					.newCacheConfigurationBuilder(String.class, String.class,
+						ResourcePoolsBuilder.newResourcePoolsBuilder()
+							.heap(1, EntryUnit.ENTRIES)
+							.disk(10, MemoryUnit.MB, true)) 
+				)
+				.build(true);
+		return persistentCacheManager;
+	}
+
+	EhCacheProviderWithXMLOrJavaBasedConfig() {
+		this(USE_XML_BASED_CONFIG);
+	}
+
+	EhCacheProviderWithXMLOrJavaBasedConfig(boolean useXmlBasedConfig) {
+		if (useXmlBasedConfig) {
+			ehCacheManager = initCacheManagerFromXMLConfig();
+			ehCacheManager.init();
+		} else
+			ehCacheManager = initCacheManagerUsingJavaAPI();
+		cache = ehCacheManager.getCache(CACHE_ALIAS, String.class, String.class);
+	}
+
+	public Cache<String, String> getEhCache() {
+		return cache;
+	}
+
+	public PersistentCacheManager getEhCacheManager() {
+		return ehCacheManager;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		ehCacheManager.close();
+	}
+}
---
+++ core/src/test/java/org/ehcache/core/config/EhCacheProviderWithXMLOrJavaBasedConfigTest.java
@@ -0,0 +1,105 @@
+package com.adazes.contribution.ehcache.issue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ehcache.Cache;
+import org.ehcache.PersistentCacheManager;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * @author Reşat SABIQ
+ */
+@RunWith(Parameterized.class)
+public class EhCacheProviderWithXMLOrJavaBasedConfigTest {
+	private static final Logger logger = Logger.getLogger(EhCacheProviderWithXMLOrJavaBasedConfigTest.class.getName());
+	private static final String[][] KEY_VALUE_PAIRS = new String[][]
+			{{"Astronaut", "Fezacı"}, {"Cosmonaut", "Fezagir"}};
+
+	private boolean useXmlBaseConfig;
+	private EhCacheProviderWithXMLOrJavaBasedConfig ehCacheProvider;
+
+	@After
+	public void afterEach() throws IOException, InterruptedException {
+		PersistentCacheManager cm = ehCacheProvider.getEhCacheManager();
+		cm.close();
+		assertTrue(getPersistenceDirectoryListing(StringUtils.EMPTY).count() > 0);
+		long fileDirListingSize = getPersistenceDirectoryListing("/file").count();
+		if (logger.isLoggable(Level.INFO))
+			logger.info("fileDirListingSize: " +fileDirListingSize);
+		assertTrue(fileDirListingSize > 0);
+	}
+
+	static Stream<Path> getPersistenceDirectoryListing(String subfolder) throws IOException {
+		String path = EhCacheProviderWithXMLOrJavaBasedConfig.getStoragePath();
+		if (StringUtils.isNotBlank(subfolder))
+			path += subfolder;
+		return Files.list(Paths.get(path));
+	}
+
+
+	@Parameters(name = "{index}: useXmlBasedConfig = {0}")
+	public static Collection<Boolean> data() {
+		return Arrays.asList(Boolean.TRUE, Boolean.FALSE);
+	}
+
+	/**
+	 * @param useXmlBaseConfig
+	 */
+	public EhCacheProviderWithXMLOrJavaBasedConfigTest(Boolean useXmlBaseConfig) {
+		this.useXmlBaseConfig = useXmlBaseConfig;
+	}
+
+	@Test
+	public final void testEhCachePutAndGet() {
+		doEhCachePutAndGet();
+	}
+
+	private final void doEhCachePutAndGet() {
+		ehCacheProvider = new EhCacheProviderWithXMLOrJavaBasedConfig(useXmlBaseConfig);
+		Cache<String, String> cache = ehCacheProvider.getEhCache();
+		if (logger.isLoggable(Level.INFO))
+			logger.warning("On 2nd execution using the same config type, true with Java-based config,"
+					+" false with XML-based one: "
+						+cache.containsKey(KEY_VALUE_PAIRS[1][0])+ " (" +cache.get(KEY_VALUE_PAIRS[1][0])+ ')');
+
+		// put:
+		cache.put(KEY_VALUE_PAIRS[0][0], KEY_VALUE_PAIRS[0][1]);
+		cache.put(KEY_VALUE_PAIRS[1][0], KEY_VALUE_PAIRS[1][1]);
+
+		String filesList = null;
+		try {
+			filesList = getPersistenceDirectoryListing(StringUtils.EMPTY).map(p -> p.getFileName())
+				.map(Object::toString).collect(Collectors.joining(StringUtils.SPACE));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (logger.isLoggable(Level.INFO))
+			logger.info("Put 2 entries into: " +cache+ ". Files: " +filesList);
+
+		// and get:
+		cache.get(KEY_VALUE_PAIRS[0][0]);
+		cache.get(KEY_VALUE_PAIRS[1][0]);
+
+		if (logger.isLoggable(Level.INFO))
+			logger.info("Got 2 entries: " +cache.containsKey(KEY_VALUE_PAIRS[1][0]));
+
+		assertEquals(KEY_VALUE_PAIRS[0][1], cache.get(KEY_VALUE_PAIRS[0][0]));
+	}
+}
