45d431f62359bd154441abd7777e5889087e25c6
Reşat SABIQ <haqer@gmx.fr>
2021-02-12 09:03:42 +0600

    Don't do logging in EhCacheProviderWithXMLOrJavaBasedConfigTest

--- xml/src/test/java/org/ehcache/xml/provider/EhCacheProviderWithXMLOrJavaBasedConfigTest.java
+++ xml/src/test/java/org/ehcache/xml/provider/EhCacheProviderWithXMLOrJavaBasedConfigTest.java
@@ -10,7 +10,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,7 +26,6 @@ import org.junit.runners.Parameterized.Parameters;
  */
 @RunWith(Parameterized.class)
 public class EhCacheProviderWithXMLOrJavaBasedConfigTest {
-	private static final Logger logger = Logger.getLogger(EhCacheProviderWithXMLOrJavaBasedConfigTest.class.getName());
 	private static final String[][] KEY_VALUE_PAIRS = new String[][]
 			{{"Astronaut", "Fezacı"}, {"Cosmonaut", "Fezagir"}};
 	private static final String SPACE = " ";
@@ -61,8 +59,6 @@ public class EhCacheProviderWithXMLOrJavaBasedConfigTest {
 		cm.close();
 		assertTrue(getPersistenceDirectoryListing(null).count() > 0);
 		long fileDirListingSize = getPersistenceDirectoryListing("/file").count();
-		if (logger.isLoggable(Level.INFO))
-			logger.info("fileDirListingSize: " +fileDirListingSize);
 		assertTrue("disk persistence directory shouldn't have been empty", fileDirListingSize > 0);
 	}
 
@@ -92,22 +88,9 @@ public class EhCacheProviderWithXMLOrJavaBasedConfigTest {
 		cache.put(KEY_VALUE_PAIRS[0][0], KEY_VALUE_PAIRS[0][1]);
 		cache.put(KEY_VALUE_PAIRS[1][0], KEY_VALUE_PAIRS[1][1]);
 
-		String filesList = null;
-		try {
-			filesList = getPersistenceDirectoryListing(null).map(p -> p.getFileName())
-				.map(Object::toString).collect(Collectors.joining(SPACE));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if (logger.isLoggable(Level.INFO))
-			logger.info("Put 2 entries into: " +cache+ ". Files: " +filesList);
-
 		// and get:
 		cache.get(KEY_VALUE_PAIRS[0][0]);
 		cache.get(KEY_VALUE_PAIRS[1][0]);
-
-		if (logger.isLoggable(Level.INFO))
-			logger.info("Got 2 entries: " +cache.containsKey(KEY_VALUE_PAIRS[1][0]));
 
 		assertEquals(KEY_VALUE_PAIRS[0][1], cache.get(KEY_VALUE_PAIRS[0][0]));
 	}
