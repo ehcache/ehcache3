8db2c411aa82dedda40b8951cec1d89fd5051fc4
Nishchay <nishchay.naresh@gmail.com>
2023-01-27 17:54:17 +0530

    issue-2494 : adding junit test

--- ehcache-107/src/test/java/org/ehcache/jsr107/UnwrapTest.java
+++ ehcache-107/src/test/java/org/ehcache/jsr107/UnwrapTest.java
@@ -15,6 +15,7 @@
  */
 package org.ehcache.jsr107;
 
+import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.core.EhcacheManager;
 import org.ehcache.event.CacheEvent;
 import org.junit.After;
@@ -28,9 +29,10 @@ import javax.cache.configuration.MutableConfiguration;
 import javax.cache.event.EventType;
 import javax.cache.spi.CachingProvider;
 
+import static org.ehcache.config.builders.ResourcePoolsBuilder.heap;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
 
 /**
  * @author rism
@@ -76,7 +78,30 @@ public class UnwrapTest {
     assertThat(cacheEntryEvent.unwrap(org.ehcache.event.CacheEvent.class), is(instanceOf(CacheEvent.class)));
     assertThat(cacheEntryEvent.unwrap(cacheEntryEvent.getClass()), is(instanceOf(Eh107CacheEntryEvent.NormalEvent.class)));
   }
+  @Test
+  public void testCacheVisibilityPostUnwrap() {
 
+    CacheManager javaxCacheManager = Caching.getCachingProvider().getCacheManager();
+
+    org.ehcache.CacheManager cacheManager = javaxCacheManager.unwrap(org.ehcache.CacheManager.class);
+    CacheConfigurationBuilder<Integer, String> cacheConfigurationBuilder = CacheConfigurationBuilder.newCacheConfigurationBuilder(Integer.class, String.class, heap(5));
+    cacheManager.createCache("jcache", cacheConfigurationBuilder);
+
+    Cache<Integer, String> javaxCache = javaxCacheManager.getCache("jcache", Integer.class, String.class);
+    assertThat(javaxCache, is(notNullValue()));
+
+    CacheManager javaxCacheManager1 = javaxCacheManager.unwrap(javax.cache.CacheManager.class);
+    Cache<Integer, String> javaxCache1 = javaxCacheManager1.getCache("jcache", Integer.class, String.class);
+    assertThat(javaxCache1, is(notNullValue()));
+
+    org.ehcache.Cache<Integer, String> cache = cacheManager.getCache("jcache", Integer.class, String.class);
+    assertThat(cache, is(notNullValue()));
+
+    cache.put(1, "one");
+    assertThat(javaxCache.get(1), is("one"));
+    assertThat(javaxCache1.get(1), is("one"));
+  }
+  
   private class EhEvent implements CacheEvent<String,String> {
     @Override
     public org.ehcache.event.EventType getType() {
