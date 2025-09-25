edfa700b7082070c076568e035e0f7bd4f0395ec
alexander-komarov-ibm <alexander.komarov@ibm.com>
2025-09-25 08:34:40 -0400

    detect-secrets baseline

---
+++ .cra/cra-scan-custom-script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "Begin CRA custom script"
+
+if [[ "${PIPELINE_DEBUG:-0}" == 1 ]]; then
+    trap env EXIT
+    env | sort
+    set -x
+fi
+
+export BUILD_ARM_TOKEN=$(get_env jfrog_func_is_token)
+export BUILD_ARM_PASSWORD=$(get_env jfrog_func_is_token)
+export BUILD_ARM_USERNAME=Ibm-webmethods-integration-server@ibm.com
+
+
+which java 
+java -version
+cat /etc/*elease*
+id
+
+# Install JDK 17
+dnf install -y java-17-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-17
+$JAVA_HOME/bin/java -version
+
+echo "End CRA custom script"
+
