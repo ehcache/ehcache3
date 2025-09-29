81791833f843a05e161bbd9df56d5a270b21c365
alexander-komarov-ibm <alexander.komarov@ibm.com>
2025-09-29 10:48:07 -0400

    install jdk 17

--- .devsecops/cra-scan-custom-script.sh
+++ .devsecops/cra-scan-custom-script.sh
@@ -12,5 +12,16 @@ export BUILD_ARM_TOKEN=$(get_env jfrog_func_is_token)
 export BUILD_ARM_PASSWORD=$(get_env jfrog_func_is_token)
 export BUILD_ARM_USERNAME=Ibm-webmethods-integration-server@ibm.com
 
+
+which java 
+java -version
+cat /etc/*elease*
+id
+
+# Install JDK 17
+dnf install -y java-17-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-17
+$JAVA_HOME/bin/java -version
+
 echo "End CRA custom script"
 
