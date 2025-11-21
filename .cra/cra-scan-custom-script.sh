d0e359c28f9bfe30a318fbd747b07f5f08c8aafc
alexander-komarov-ibm <alexander.komarov@ibm.com>
2025-11-21 14:08:22 -0500

    SSP onboarding pass 1

--- .cra/cra-scan-custom-script.sh
+++ .cra/cra-scan-custom-script.sh
@@ -12,17 +12,7 @@ export BUILD_ARM_TOKEN=$(get_env jfrog_func_is_token)
 export BUILD_ARM_PASSWORD=$(get_env jfrog_func_is_token)
 export BUILD_ARM_USERNAME=Ibm-webmethods-integration-server@ibm.com
 
-
-which java 
-java -version
-cat /etc/*elease*
-id
-
-# Install JDK 17
-dnf install -y java-17-openjdk-devel
-export JAVA_HOME=/usr/lib/jvm/java-17
-$JAVA_HOME/bin/java -version
-
+source "$(dirname "${BASH_SOURCE[0]}")/install-jdk.sh"
 
 # debug: dump all scripts
 find /opt/commons/compliance-checks | while read filename ; do
